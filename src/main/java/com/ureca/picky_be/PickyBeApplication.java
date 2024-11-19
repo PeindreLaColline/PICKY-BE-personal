ed0702d783b4f24ef69bd81ea7f65ed73e79e72f
Peindre La Colline <PeindreLaColline@gmail.com>
2024-11-19 10:06:01 +0900

    feat/project init

---
+++ src/main/java/com/ureca/picky_be/PickyBeApplication.java
@@ -0,0 +1,13 @@
+package com.ureca.picky_be;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PickyBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PickyBeApplication.class, args);
+	}
+
+}
