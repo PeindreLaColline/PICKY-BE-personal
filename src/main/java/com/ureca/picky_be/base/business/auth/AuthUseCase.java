08969fb19db92ec6a3f5bef520f469bbca0fa029
Peindre La Colline <PeindreLaColline@gmail.com>
2024-11-21 10:16:05 +0900

    Fix: 폴더 구조 생성 위해 파일 임시로 생성

---
+++ src/main/java/com/ureca/picky_be/base/business/auth/AuthUseCase.java
@@ -0,0 +1,9 @@
+package com.ureca.picky_be.base.business.auth;
+
+public interface AuthUseCase {
+    //TODO: 수정 필요
+/*    LoginResp login(LoginReq req);
+    void logout();
+    SignupResp signup(SignupReq req);
+    void deleteAccount();*/
+}
