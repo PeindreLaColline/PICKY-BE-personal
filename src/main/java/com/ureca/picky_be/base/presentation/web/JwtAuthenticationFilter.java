0166440c1445bd6b1266b424ba325b6bb6930c31
Peindre La Colline <PeindreLaColline@gmail.com>
2024-11-24 01:43:13 +0900

    refact: 개발 편의를 위해 security 수정

--- src/main/java/com/ureca/picky_be/base/presentation/web/JwtAuthenticationFilter.java
+++ src/main/java/com/ureca/picky_be/base/presentation/web/JwtAuthenticationFilter.java
@@ -5,6 +5,8 @@ import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -12,6 +14,7 @@ import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import java.io.IOException;
+import java.util.List;
 
 @Component
 public class JwtAuthenticationFilter extends OncePerRequestFilter {
@@ -32,10 +35,13 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
 
         if (token != null && jwtTokenProvider.validateToken(token)) {
             Long userId = jwtTokenProvider.getUserId(token);
+            String role = jwtTokenProvider.getRole(token); // 추가: 역할 추출
+
             UserDetails userDetails = userDetailsService.loadUserByUsername(String.valueOf(userId));
 
+            List<GrantedAuthority> authorities = List.of(new SimpleGrantedAuthority(role)); // 역할 추가
             UsernamePasswordAuthenticationToken authentication = new UsernamePasswordAuthenticationToken(
-                    userDetails, null, userDetails.getAuthorities());
+                    userDetails, null, authorities);
             SecurityContextHolder.getContext().setAuthentication(authentication);
         }
 
