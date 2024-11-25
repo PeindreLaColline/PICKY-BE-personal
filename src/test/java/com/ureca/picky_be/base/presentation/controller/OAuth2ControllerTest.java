1b9bdcca357a7366e3bdd9a64ff8d9f2e98e5607
Peindre La Colline <PeindreLaColline@gmail.com>
2024-11-25 14:24:06 +0900

    test: 테스트 코드 주석처리

--- src/test/java/com/ureca/picky_be/base/presentation/controller/OAuth2ControllerTest.java
+++ src/test/java/com/ureca/picky_be/base/presentation/controller/OAuth2ControllerTest.java
@@ -4,16 +4,15 @@ import com.ureca.picky_be.base.presentation.controller.auth.OAuth2Controller;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.ActiveProfiles;
 
 //@ActiveProfiles("local")
 @SpringBootTest
 public class OAuth2ControllerTest {
-
+/*
     @Autowired
     OAuth2Controller controller;
 
-    /* @Test
+     @Test
     void getNaverLoginUrlTest() {
         String url = controller.getNaverLoginUrl().getBody();
         System.out.println(url);
