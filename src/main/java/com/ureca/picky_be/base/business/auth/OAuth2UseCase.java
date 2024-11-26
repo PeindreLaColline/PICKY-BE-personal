38d88e78e497d838d327e35131a79f3a594a3c79
Peindre La Colline <PeindreLaColline@gmail.com>
2024-11-26 23:45:51 +0900

    feat: add delete to usecase

--- src/main/java/com/ureca/picky_be/base/business/auth/OAuth2UseCase.java
+++ src/main/java/com/ureca/picky_be/base/business/auth/OAuth2UseCase.java
@@ -1,7 +1,11 @@
 package com.ureca.picky_be.base.business.auth;
 
+import com.ureca.picky_be.base.business.auth.dto.DeleteUserReq;
+import com.ureca.picky_be.base.business.auth.dto.OAuth2Token;
+
 public interface OAuth2UseCase {
 
     String getLoginUrl();
     String getUserInfo(String state, String code);
+    String deleteAccount(DeleteUserReq req);
 }
