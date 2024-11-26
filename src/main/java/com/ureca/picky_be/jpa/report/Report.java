639dd42d504450cbde53c965c276667e529bf31b
Sean Lee <61014396+lsh981127@users.noreply.github.com>
2024-11-26 15:02:02 +0900

    feat : 신고 부분 수정

--- src/main/java/com/ureca/picky_be/jpa/report/Report.java
+++ src/main/java/com/ureca/picky_be/jpa/report/Report.java
@@ -17,9 +17,8 @@ public class Report extends BaseEntity {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @ManyToOne(fetch=FetchType.LAZY)
-    @JoinColumn(name = "user_id", nullable = false)
-    private User user;
+    @Column(name = "user_id", nullable = false)
+    private Long userId;
 
     @Column(nullable = false)
     private Long reviewId;
