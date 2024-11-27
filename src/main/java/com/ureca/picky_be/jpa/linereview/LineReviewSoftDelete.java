eef5d0b7e6ba5ce828e45c482fdcea4f4e84a647
Sean Lee <61014396+lsh981127@users.noreply.github.com>
2024-11-27 16:36:46 +0900

    feat : 한줄평 논리적 삭제 엔티티 생성

---
+++ src/main/java/com/ureca/picky_be/jpa/linereview/LineReviewSoftDelete.java
@@ -0,0 +1,32 @@
+package com.ureca.picky_be.jpa.linereview;
+
+import com.ureca.picky_be.jpa.config.BaseEntity;
+import jakarta.persistence.*;
+import lombok.Getter;
+import org.hibernate.annotations.ColumnDefault;
+
+@Getter
+@Entity
+public class LineReviewSoftDelete extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name="line_review_id", nullable = false)
+    private Long lineReviewId;
+
+    @Column(name="user_id", nullable = false)
+    private Long userId;
+
+    @Column(name="movie_id", nullable = false)
+    private Long movieId;
+
+    @ColumnDefault("0.0")
+    private int rating;
+
+    @Column(nullable = false)
+    private String context;
+
+
+}
