0e6485be1d83c4ef6ae1c234d1923d6d35f9cefe
chaewon KIM <158552381+PeindreLaColline@users.noreply.github.com>
2024-11-28 17:49:38 +0900

    Update GoogleManager.java

--- src/main/java/com/ureca/picky_be/base/implementation/auth/GoogleManager.java
+++ src/main/java/com/ureca/picky_be/base/implementation/auth/GoogleManager.java
@@ -124,7 +124,7 @@ public class GoogleManager {
         LoginUserResp resp = new LoginUserResp(oAuth2Token, email, jwt);
         try {
             restClient
-                    .get()
+                    .post()
                     .uri(buildFrontendUrl())
                     .header("Content-Type", "application/json")
                     .body(resp)
