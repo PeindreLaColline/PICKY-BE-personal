35fe7d1bcb2486713853344c2d5c8d202b437f8e
Peindre La Colline <PeindreLaColline@gmail.com>
2024-11-29 17:10:45 +0900

    feat: 에러코드 추가

--- src/main/java/com/ureca/picky_be/global/success/SuccessCode.java
+++ src/main/java/com/ureca/picky_be/global/success/SuccessCode.java
@@ -9,6 +9,7 @@ public enum SuccessCode {
     // User
     REQUEST_FRONT_SUCCESS(200, "프론트에 요청 완료"),
     REQUEST_DELETE_ACCOUNT_SUCCESS(200, "유저 계정 삭제 완료"),
+    UPDATE_USER_SUCCESS(200, "유저 정보 업데이트 완료"),
 
     //Movie
     CREATE_MOVIE_SUCCESS(201, "영화 생성 완료"),
