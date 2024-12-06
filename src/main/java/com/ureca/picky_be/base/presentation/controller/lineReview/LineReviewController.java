af5576fdecff2b35175fb4229a27a4bf6ff75e27
Peindre La Colline <PeindreLaColline@gmail.com>
2024-12-06 20:02:20 +0900

    fix: 경로 수정

--- src/main/java/com/ureca/picky_be/base/presentation/controller/lineReview/LineReviewController.java
+++ src/main/java/com/ureca/picky_be/base/presentation/controller/lineReview/LineReviewController.java
@@ -3,18 +3,14 @@ package com.ureca.picky_be.base.presentation.controller.lineReview;
 
 import com.ureca.picky_be.base.business.lineReview.LineReviewUseCase;
 import com.ureca.picky_be.base.business.lineReview.dto.*;
-import com.ureca.picky_be.global.exception.CustomException;
-import com.ureca.picky_be.jpa.lineReview.LineReview;
 import com.ureca.picky_be.jpa.lineReview.SortType;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Slice;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.time.LocalDate;
 import java.time.LocalDateTime;
 
 @RestController
