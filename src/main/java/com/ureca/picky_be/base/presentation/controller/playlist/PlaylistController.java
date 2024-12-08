46f06e156479b09a7084e546c96bb177112443e8
Peindre La Colline <PeindreLaColline@gmail.com>
2024-12-09 01:43:43 +0900

    feat: 스웨거 api 설명 추가

--- src/main/java/com/ureca/picky_be/base/presentation/controller/playlist/PlaylistController.java
+++ src/main/java/com/ureca/picky_be/base/presentation/controller/playlist/PlaylistController.java
@@ -2,12 +2,10 @@ package com.ureca.picky_be.base.presentation.controller.playlist;
 
 import com.ureca.picky_be.base.business.playlist.PlaylistUseCase;
 import com.ureca.picky_be.base.business.playlist.dto.GetPlaylistResp;
+import io.swagger.v3.oas.annotations.Operation;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Slice;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
@@ -16,6 +14,7 @@ public class PlaylistController {
 
     private final PlaylistUseCase playlistUseCase;
 
+    @Operation(summary = "전체 플레이리스트 조회", description = "플레이리스트 3개씩 페이징 조회. size는 필요한 개수만큼(ex: 메인페이지라면 3개). last-playlist-id는 리스트의 가장 마지막에 있는 플레이리스트의 id")
     @GetMapping("/all")
     public Slice<GetPlaylistResp> getPlaylist(
             @RequestParam(value = "last-playlist-id", required = false) Long lastPlaylistId,
