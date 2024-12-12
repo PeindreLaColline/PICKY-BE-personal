755d2deff6e30cb8138c33ddd7948357be2934c9
Sean Lee <61014396+lsh981127@users.noreply.github.com>
2024-12-12 16:03:50 +0900

    fix : user 불필요 코드 제거

--- src/main/java/com/ureca/picky_be/base/business/user/UserUseCase.java
+++ src/main/java/com/ureca/picky_be/base/business/user/UserUseCase.java
@@ -1,13 +1,9 @@
 package com.ureca.picky_be.base.business.user;
 
-import com.ureca.picky_be.base.business.lineReview.dto.ReadLineReviewResp;
 import com.ureca.picky_be.base.business.user.dto.GetNicknameValidationResp;
 import com.ureca.picky_be.base.business.user.dto.GetUserResp;
 import com.ureca.picky_be.base.business.user.dto.RegisterUserReq;
-import com.ureca.picky_be.base.business.user.dto.UserLineReviewsReq;
 import com.ureca.picky_be.global.success.SuccessCode;
-import org.springframework.data.domain.PageRequest;
-import org.springframework.data.domain.Slice;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
