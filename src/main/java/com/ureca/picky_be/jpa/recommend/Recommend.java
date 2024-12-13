2ddbd9cd7169d714a572313655fca4a1422817a3
YaeJinKwon <kwonreally@naver.com>
2024-12-13 23:38:21 +0900

    feat: Recommend entity 생성

---
+++ src/main/java/com/ureca/picky_be/jpa/recommend/Recommend.java
@@ -0,0 +1,27 @@
+package com.ureca.picky_be.jpa.recommend;
+
+import com.ureca.picky_be.jpa.config.BaseEntity;
+import jakarta.persistence.*;
+import lombok.*;
+
+@Getter
+@Entity
+@Builder
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Recommend extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name="user_id", nullable = false)
+    private Long userId;
+
+    @Column(name="movie_id", nullable = false)
+    private Long movieId;
+
+    @Column(nullable = false)
+    private Long similarity;
+
+}
