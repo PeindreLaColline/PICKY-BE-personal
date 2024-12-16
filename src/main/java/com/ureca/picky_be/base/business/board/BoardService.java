9931767e9c82f9392faa23fd4aa693455f19b2e6
chaewon KIM <158552381+PeindreLaColline@users.noreply.github.com>
2024-12-16 19:45:25 +0900

    Update BoardService.java

--- src/main/java/com/ureca/picky_be/base/business/board/BoardService.java
+++ src/main/java/com/ureca/picky_be/base/business/board/BoardService.java
@@ -86,7 +86,7 @@ public class BoardService implements BoardUseCase {
         Slice<BoardProjection> recentBoards = boardManager.getRecentMovieBoards(userId, lastBoardId, pageable);
         List<String> profileUrls = recentBoards.getContent().stream()
                 .map(BoardProjection::getWriterProfileUrl)
-                .map(profileManager::getPresignedUrl)
+                .map(url -> url != null ? profileManager.getPresignedUrl(url) : null)
                 .toList();
         List<Long> boardIds = recentBoards.getContent().stream()
                 .map(BoardProjection::getBoardId)
