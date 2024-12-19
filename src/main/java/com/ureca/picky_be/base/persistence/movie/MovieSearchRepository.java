b9b3c0bcaf36b8605aa47fc695c800da283c610c
YaeJinKwon <kwonreally@naver.com>
2024-12-19 16:51:27 +0900

    fix : 엘라스틱 서치 수정

--- src/main/java/com/ureca/picky_be/base/persistence/movie/MovieSearchRepository.java
+++ src/main/java/com/ureca/picky_be/base/persistence/movie/MovieSearchRepository.java
@@ -6,22 +6,18 @@ import org.springframework.data.elasticsearch.repository.ElasticsearchRepository
 import java.util.List;
 
 public interface MovieSearchRepository extends ElasticsearchRepository <MovieDocument, Long> {
-    // Match Phrase Prefix 쿼리
     @Query("""
-    {
-        "bool": {
-          "must": [
-            {
-              "prefix": {
-                "title": {
-                  "value": "?0"
-                }
-              }
+        {
+          "match": {
+            "title": {
+              "query": "?0",
+              "analyzer": "mixed_ngram_analyzer"
             }
-          ]
+          }
         }
-    }
-    """)
+        """)
     List<MovieDocument> findByTitle(String title);
+
+
 
 }
