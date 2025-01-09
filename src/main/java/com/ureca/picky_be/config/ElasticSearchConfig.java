a9c7771e7cd8ef21280060d266391b8ac0a7c2e0
Peindre La Colline <PeindreLaColline@gmail.com>
2025-01-09 23:17:26 +0900

    feat: elastic search local setting

--- src/main/java/com/ureca/picky_be/config/ElasticSearchConfig.java
+++ src/main/java/com/ureca/picky_be/config/ElasticSearchConfig.java
@@ -1,19 +1,26 @@
 package com.ureca.picky_be.config;
 
+import lombok.RequiredArgsConstructor;
+import org.apache.http.ssl.SSLContexts;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.elasticsearch.client.ClientConfiguration;
 import org.springframework.data.elasticsearch.client.elc.ElasticsearchConfiguration;
 import org.springframework.data.elasticsearch.repository.config.EnableElasticsearchRepositories;
+import org.springframework.web.client.RestClient;
+
+import javax.net.ssl.SSLContext;
+import java.io.File;
 
 @Configuration
 @EnableElasticsearchRepositories
+@RequiredArgsConstructor
 public class ElasticSearchConfig extends ElasticsearchConfiguration {
+
+    RestClient restClient = RestClient.create();
+
     @Value("${spring.data.elasticsearch.host}")
     private String host;
-
-    @Value("${spring.data.elasticsearch.api-key}")
-    private String apiKey;
 
     @Value("${spring.data.elasticsearch.username}")
     private String username;
@@ -21,12 +28,27 @@ public class ElasticSearchConfig extends ElasticsearchConfiguration {
     @Value("${spring.data.elasticsearch.password}")
     private String password;
 
+    @Value("${spring.data.elasticsearch.ssl.trust-store}")
+    private String trustStorePath;
+
+    @Value("${spring.data.elasticsearch.ssl.trust-store-password}")
+    private String trustStorePassword;
+
     @Override
     public ClientConfiguration clientConfiguration() {
-        return ClientConfiguration.builder()
-                .connectedTo(host)
-                .usingSsl()
-                .withBasicAuth(username, password)
-                .build();
+        try {
+            SSLContext sslContext = SSLContexts.custom()
+                    .loadTrustMaterial(new File(trustStorePath), trustStorePassword.toCharArray())
+                    .build();
+
+            return ClientConfiguration.builder()
+                    .connectedTo(host)
+                    .usingSsl(sslContext)
+                    .withBasicAuth(username, password)
+                    .build();
+        } catch (Exception e) {
+            throw new RuntimeException("Failed to configure SSL for Elasticsearch", e);
+        }
     }
+
 }
